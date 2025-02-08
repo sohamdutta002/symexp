5230c86756233ab9a00ce50efc26255c6ff6ebd3
Soham Dutta <duttasoham2222@gmail.com>
2025-02-08 13:25:19 +0530

    initial commit using flex

---
+++ src/Components/SymphonyOfExperiences.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./styles.css"
+import { NumberTicker } from './magicui/number-ticker'
+import Image from 'next/image'
+
+const SymphonyOfExperiences = () => {
+  return (
+    <div className='symexp font-c text-8xl text-white flex flex-col items-center justify-center'>
+        <h1>Symphony Of Experiences</h1>
+        <div className="w-full h-full flex">
+            <div className="w-[50vw] overflow-hidden flex flex-col items-center justify-center em">
+                <Image src="https://utfs.io/f/3d7e218e-f420-446b-b3ce-f66833b46a65-bles7u.png" alt="Five Elementals" width={500} height={500} className='spin' />
+            </div>
+            <div className="w-[50vw] h-full flex flex-wrap gap-6 justify-between pr-20">
+                <div className="flex flex-col justify-center text-3xl font-serif">
+                    <NumberTicker value={6} className='text-[8rem] mb-10 font-bold font-c' /> Years
+                </div>
+                <div className="flex flex-col justify-center text-3xl font-serif">
+                    <NumberTicker value={50} className='text-[8rem] mb-10 font-bold font-c' /> Events
+                </div>
+                <div className="flex flex-col justify-center text-3xl font-serif">
+                    <NumberTicker value={35} className='text-[8rem] mb-10 font-bold font-c' /> Partners
+                </div>
+                <div className="flex flex-col justify-center text-3xl font-serif">
+                    <NumberTicker value={6000} className='text-[8rem] mb-10 font-bold font-c' /> Participants
+                </div>
+                <div className="flex flex-col justify-center text-3xl font-serif">
+                    <NumberTicker value={10000} className='text-[8rem] mb-10 font-bold font-c' /> Footfall
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SymphonyOfExperiences
+
+
+
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import SymphonyOfExperiences from "@/Components/SymphonyOfExperiences";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <SymphonyOfExperiences />
+  );
+}
